29a9a05b1691efdee9e0a4f6e7884308da3bf013
ritaguowu <ritaguowu@gmail.com>
2022-09-05 19:19:00 -0400

    10 Add points for child

--- application/api/routes.py
+++ application/api/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from application.models import Parent, Child
+from application.models import Parent, Child, Point
 from flask_jwt_extended import jwt_required
 
 
@@ -20,15 +20,14 @@ def auth():
     return Parent().signIn()
 
 #Get user information by token and parentId. For the auto login purpose.
-@api.route('/user', methods=['GET'])
-def getUser():
+@api.route('/parent', methods=['GET'])
+def getParentById():
     token = request.args.get('token', None)
-    print(token)
     parentId = request.args.get('parentId', None)
     if parentId is None:
         return jsonify({"error":"Please provide the parentId" }), 403
     else:
-        return Parent().getUserById(token, parentId)
+        return Parent().getParentById(token, parentId)
 
 
 # Get kids information for the parent
@@ -47,10 +46,11 @@ def getKids():
 def addKid():
     kid_name = request.args.get('kidName')
     parentId = request.args.get('parentId')
+    token = request.headers["Authorization"].split(" ")[1]
     if parentId is None:
         return jsonify({"error":"Please provide the parentId" }), 403
     else:
-        return Child().createKid(kid_name, parentId)
+        return Child().createKid(kid_name, parentId, token)
 
 #Update the parent information
 @api.route('/parent', methods=['PUT'])
@@ -65,6 +65,7 @@ def updateParent():
 
 
 @api.route('/kid', methods=['DELETE'])
+@jwt_required()
 def removeKid():
     kidId = request.args.get('kidId')
     # if None in (email, kid_name):
@@ -74,27 +75,39 @@ def removeKid():
         return Child().deleteKid(kidId)
 
 
-# @api.route('/user', methods=['GET'])
-# @jwt_required()
-# def getUser():
-#     token = request.headers["Authorization"].split(" ")[1]
-#     print(token)
-#     parentId = request.args.get('parentId')
-#     if parentId is None:
-#         return jsonify({"error":"Please provide the parentId" }), 403
-#     else:
-#         return Parent().getUserById(token, parentId)
-
-
-
-
-@api.route('/parent', methods=['GET'])
-def getParent():
-    email = request.args.get('email') 
-    if email is None:
-        return jsonify({"error":"Please provide the email" }), 410
+@api.route('/point', methods=['PUT'])
+@jwt_required()
+def updatePoint():
+    kidId = request.args.get('kidId')
+    points = request.args.get('points')
+    if kidId is None:
+        return jsonify({"error":"Please provide the kid's id" }), 409
     else:
-        return Parent().getParentByEmail(email)
+        return Point().updatePoint(kidId, points)
+
+
+
+@api.route('/kid', methods=['GET'])
+@jwt_required()
+def getKidById():
+    token = request.headers["Authorization"].split(" ")[1]
+    print(token)
+    kidId = request.args.get('kidId')
+    if kidId is None:
+        return jsonify({"error":"Please provide the parentId" }), 403
+    else:
+        return Child().getKidById(kidId, token)
+
+
+
+
+# @api.route('/parent', methods=['GET'])
+# def getParent():
+#     email = request.args.get('email') 
+#     if email is None:
+#         return jsonify({"error":"Please provide the email" }), 410
+#     else:
+#         return Parent().getParentByEmail(email)
 
 
 @api.route('/kid', methods=['PUT'])
--- application/models.py
+++ application/models.py
@@ -58,7 +58,7 @@ class Parent:
             "image": "",
             "parentId": "",
             # "parentId": request.json['parentId'],
-            "access_token": ""
+            "access_token": "",
         }
 
         # Encrypt the password
@@ -83,8 +83,8 @@ class Parent:
                 user["access_token"] = create_access_token(identity=user['email'])
                 filter = {'email': user['email']}
                 db.users.update_one(filter, {"$set": {"access_token":user["access_token"]}})
-                user = db.users.find_one({"email": request.json['email']})
-                return jsonify({"success":"Login successfully", "user": user}), 200
+                parent = db.users.find_one({"email": request.json['email']})
+                return jsonify({"success":"Login successfully", "user": parent}), 200
             else:
                 return jsonify({"error":"Password is not correct" }), 401
         else:
@@ -94,15 +94,16 @@ class Parent:
         user = db.users.find_one({"email":email})
         return jsonify({"success":"Success", 'user': user }), 200
 
-    #Get user information by token and parentId 
-    def getUserById(self, token, parentId):
-        user = db.users.find_one({"_id":parentId})
-        print(user)
-        if user['access_token'] == token:
-            user["access_token"] = create_access_token(identity=user['email'])
+    #Get user information by token and userId 
+    def getParentById(self, token, parentId):
+        parent = db.users.find_one({"_id":parentId})
+        print(parent)
+        if parent['access_token'] == token:
+            parent["access_token"] = create_access_token(identity=parent['email'])
             filter = {"_id":parentId}
-            db.users.update_one(filter, {"$set": {"access_token":user["access_token"]}})
-            return jsonify({"success":"Success", 'user': user }), 200
+            db.users.update_one(filter, {"$set": {"access_token":parent["access_token"]}})
+            parent = db.users.find_one({"_id":parentId})
+            return jsonify({"success":"Success", 'user': parent }), 200
         else:
             return jsonify({"error":"Invalid login" }), 402
         
@@ -126,23 +127,36 @@ class Parent:
 class Child(Parent):
 
     # create kid for the parent
-    def createKid(self, kid_name, parentId):
+    def createKid(self, kid_name, parentId, token):
         print(request)
         
         myquery={"$and":[{"parentId":parentId, "user_name":kid_name}]}
-        user = db.users.find_one(myquery)
+        kid = db.users.find_one(myquery)
 
-        if user:
+        if kid:
             return jsonify({"error":"User exitst"}), 400
         else:
         #Create the user object
-            user = {
+            kid = {
                 "_id": uuid.uuid4().hex,
                 "user_name": kid_name,
+                "image": "",
                 "parentId": parentId,
+                "points": 0,
+                "access_token": token,
             }
-            db.users.insert_one(user)
-            return jsonify({"success":"Kid login successfully", "user": user}), 200
+            db.users.insert_one(kid)
+            return jsonify({"success":"Kid login successfully", "user": kid}), 200
+
+    def getKidById(self, kidId, token):
+        kid = db.users.find_one({"_id":kidId})
+        print(kid)
+        if kid['access_token'] == token:
+            return jsonify({"success":"Success", 'kid': kid }), 200
+        else:
+            return jsonify({"error":"Invalid login" }), 402
+        
+    
 
     # Get kids information for the parent
     def getKidsByParentId(self, parentId):
@@ -158,19 +172,30 @@ class Child(Parent):
             return jsonify({"success":"Delete successfully"}), 200
         else:
             return jsonify({"error":"User not found"}), 410
-   
+ 
 
-
-    def updateKid(self, email, kid_name):
-        parent = db.users.find_one({"email":email})
-        user_name=request.json['user_name']
-
-        print(parent['_id'], kid_name)
-        image=request.json['image']
-        myquery={"$and":[{"parentId":parent['_id'], "user_name":kid_name}]}
-        newvalues={"$set": {"user_name":user_name, "image":image}}
-        db.users.find_one_and_update(myquery, newvalues)
+class Point:
+    def updatePoint(self, kidId, points):
+        kid = db.users.find_one({"_id":kidId})
+        if kid:
+            filter = {"_id":kidId}
+            db.users.update_one(filter, {"$set": {"points":points}})
         # user = db.users.find_one(myquery)
-        user = db.users.find_one({"$or":[{"parentId":parent['_id'], "user_name":user_name}]})
-        return jsonify({"success":"Update kid successfully", "user": user}), 200
+        user = db.users.find_one({"_id":kidId})
+        return jsonify({"success":"Update kid point successfully", "user": user}), 200
+
+
+    # def updateKid(self, email, kid_name):
+    #     parent = db.users.find_one({"email":email})
+    #     user_name=request.json['user_name']
+
+    #     print(parent['_id'], kid_name)
+    #     image=request.json['image']
+    #     myquery={"$and":[{"parentId":parent['_id'], "user_name":kid_name}]}
+    #     newvalues={"$set": {"user_name":user_name, "image":image}}
+    #     db.users.find_one_and_update(myquery, newvalues)
+    #     # user = db.users.find_one(myquery)
+    #     user = db.users.find_one({"$or":[{"parentId":parent['_id'], "user_name":user_name}]})
+    #     return jsonify({"success":"Update kid successfully", "user": user}), 200
+
 
